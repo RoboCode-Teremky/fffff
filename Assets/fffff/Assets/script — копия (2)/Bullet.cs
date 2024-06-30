91b7e1f8285c1fd75c0ff39dccd61cc47f69368f
Олександр Куцик <kutsik.alexander@gmail.com>
2024-06-30 11:35:33 +0300

    Fix weapon

--- Assets/fffff/Assets/script — копия (2)/Bullet.cs
+++ Assets/fffff/Assets/script — копия (2)/Bullet.cs
@@ -13,7 +13,7 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     
+     Destroy(gameObject, lifetime);
     }
 
     // Update is called once per frame
